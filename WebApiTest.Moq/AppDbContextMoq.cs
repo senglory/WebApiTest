50f4cbd3c1e6f2510acddb6e17b35c6762ec286b
NS <senglory@gmail.com>
2018-06-13 08:59:42 +0300

    - code cleanup

---
+++ WebApiTest.Moq/AppDbContextMoq.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WebApiTest.Interfaces;
+
+using Moq;
+
+namespace WebApiTest.Moq
+{
+    public class AppDbContextMoq : IAppDbContext
+    {
+        public Guid Add(Asset dto)
+        {
+            var mock = new Mock<IAppDbContext>();
+
+
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public Asset FindById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public QueryResult GetResults(string filterByValue, int start, int length, Dictionary<string, string> orderBy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetTotalCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Asset dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WebApiTest.Moq/Properties/AssemblyInfo.cs
+++ WebApiTest.Moq/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebApiTest")]
+[assembly: AssemblyTitle("WebApiTest.Moq")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebApiTest")]
+[assembly: AssemblyProduct("WebApiTest.Moq")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("4e6629a9-272b-48d1-a7b1-8e7851563c83")]
+[assembly: Guid("8904f094-9139-46cb-84a0-4e47e4715131")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
